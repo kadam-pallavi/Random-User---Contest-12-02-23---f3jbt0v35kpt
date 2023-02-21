9c134ac2b8ce89b09cd53b0cf677112645409cf5
kadam-pallavi <117823209+kadam-pallavi@users.noreply.github.com>
2023-02-22 00:12:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,40 @@
-import { useEffect, useState } from "react";
-import "./App.css";
-function App() {
-  const [data, setdata] = useState([]);
-  const [displayData, setDisplayData] = useState("");
+const imageElement = document.getElementById('image');
+const nameElement = document.getElementById('name');
+const ageButton = document.getElementById('age');
+const emailButton = document.getElementById('email');
+const phoneButton = document.getElementById('phone');
+const additionalInfoElement = document.getElementById('additional-info');
+const getUserButton = document.getElementById('getUser');
 
-  useEffect(() => {
-    fetch("https://randomuser.me/api/")
-      .then((response) => response.json())
-      .then((data) => setdata(data.results && data.results[0]));
-  }, []);
-  return (
-    <div id="main" className="App">
-      <div className="profile">
-        <img src={data?.picture?.large} alt="" />
-        <span className="name">
-          {data?.name?.first + " "} {data?.name?.last}
-        </span>
-      </div>
-      <div className="buttons">
-        <button onClick={() => setDisplayData(data?.dob?.age)}>Age</button>
-        <button onClick={() => setDisplayData(data?.email)}>Email</button>
-        <button onClick={() => setDisplayData(data?.phone)}>Phone</button>
-      </div>
-      <section>
-        <h1>Additinal Info</h1>
-        <span>{displayData}</span>
-      </section>
-    </div>
-  );
+const RANDOM_USER_API = "https://randomuser.me/api/";
+let age = 24;
+let email = "adarshsharma42876@gmail.com";
+let phone = "(+91) 9598210693";
+
+function getRandomUser(){
+     return fetch(RANDOM_USER_API).then(res => res.json()).then(res => res.results[0]);
 }
 
-export default App;
+async function renderRandomUser(){
+     let randomUser = await getRandomUser();
+     imageElement.src = randomUser.picture.large;
+     nameElement.innerText = `${randomUser.name.first} ${randomUser.name.last}`;
+     age = randomUser.dob.age;
+     email = randomUser.email;
+     phone = randomUser.phone;
+     console.log(randomUser);
+}
+
+renderRandomUser();
+
+ageButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = age;
+});
+emailButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = email;
+});
+phoneButton.addEventListener('click', _ => {
+     additionalInfoElement.innerText = phone;
+});
+
+getUserButton.addEventListener('click', renderRandomUser);
